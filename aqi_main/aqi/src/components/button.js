18faca915f1db01f57eb1c73cbb0205d71d87f35
dhruv <dhruvsaini0904@gmail.com>
2023-05-19 12:16:11 +0530

    adding aqi and weather app init

---
+++ aqi_main/aqi/src/components/button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FrontPage from './FrontPage';
+
+function App() {
+  return (
+    <div className="App">
+      <FrontPage />
+    </div>
+  );
+}
+
+export default App;
